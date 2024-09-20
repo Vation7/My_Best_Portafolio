c2938c44ca7f88f68d10b817130a00d0c6e5c279
Miguel <gawerne60@icloud.com>
2024-09-20 15:58:02 -0600

    new try

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://github.com/Vation7" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a href="https://stackoverflow.com/" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+    </footer>
+  );
+}
+
+export default Footer;
